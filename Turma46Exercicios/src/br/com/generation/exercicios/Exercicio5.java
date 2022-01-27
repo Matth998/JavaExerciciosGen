97f1dd6095e4f14faf2e9ea65d4d892aee501dc3
Matth998 <matheusoliveira.3125@gmail.com>
2022-01-27 10:09:54 -0300

    Atualização de validação

--- Turma46Exercicios/src/br/com/generation/exercicios/Exercicio5.java
+++ Turma46Exercicios/src/br/com/generation/exercicios/Exercicio5.java
@@ -11,28 +11,41 @@ public class Exercicio5 {
 		Scanner leia = new Scanner(System.in);
 		DecimalFormat formatado = new DecimalFormat("0.0");
 		
-		System.out.println("Digite as notas do aluno.");
 		System.out.println("Nota 1: ");
 		n1 = leia.nextDouble();
+		
+		while(n1 < 0 || n1 > 10) {
+			
+			System.out.println("Nota invalida, Digite Novamente!");
+			System.out.println("Nota 1: ");
+			n1 = leia.nextDouble();
+			
+		}
 		
 		System.out.println("Nota 2: ");
 		n2 = leia.nextDouble();
 		
-		System.out.println("Nota 3: ");
-		n3 = leia.nextDouble();
-		
-		if(n1 > 0 && n2 > 0 && n3 > 0 && n1 <= 10 && n2 <= 10 && n3 <= 10) {
-		
-			media = (n1*2 + n2 * 3 + n3 * 5)/(2+3+5);
+		while(n2 <0 || n2 > 10){
 			
-			System.out.println("A média do aluno é: " + formatado.format(media));
-		
-		}else {
-			
-			System.out.println("Você adicinou uma nota inválida, por favor revise!!");
+			System.out.println("Nota invalida, Digite Novamente!");
+			System.out.println("Nota 2: ");
+			n2 = leia.nextDouble();
 			
 		}
 		
+		System.out.println("Nota 3: ");
+		n3 = leia.nextDouble();
+		
+		while(n3 < 0 || n3 > 10){
+			
+			System.out.println("Nota invalida, Digite Novamente!");
+			System.out.println("Nota 3: ");
+			n3 = leia.nextDouble();
+		}
+		
+		media = (n1*2 + n2 * 3 + n3 * 5)/(2+3+5);
+		System.out.println("A média do aluno é: " + formatado.format(media));
+				
 		leia.close();
 
 	}
